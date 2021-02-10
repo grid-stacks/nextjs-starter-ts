9f861518b97a56a057bc1bb78f22a3e107f6803e
dhnchandan <php.chandan@gmail.com>
2021-02-10 17:47:09 +0600

    feat(redux): :construction: Implement redux store
    Redux tookit implemeted for redus store. Some minor configurations and chages are required.
    
    Ref: #6

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,41 @@
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+import { Store } from "@reduxjs/toolkit";
+
+import { useStore } from "@store/store";
+// import withRedux from "next-redux-wrapper";
+// import getStore, { useStore } from "@store/store";
+
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+export interface MyAppProps {
+	Component: ReactNode;
+	store: Store;
+	pageProps: object;
 }
+
+const MyApp = ({ Component, pageProps }) => {
+	const store = useStore();
+
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />;
+		</Provider>
+	);
+};
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+	console.log(ctx);
+	const pageProps = Component.getInitialProps
+		? await Component.getInitialProps(ctx)
+		: {};
+
+	//Anything returned here can be accessed by the client
+	return { pageProps: pageProps };
+};
+
+// const makeStore = () => getStore;
+
+// export default withRedux(makeStore)(MyApp);
 
 export default MyApp;
---
+++ src/store/store.ts
@@ -0,0 +1,71 @@
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import {
+	configureStore,
+	getDefaultMiddleware,
+	Action,
+	Store,
+	applyMiddleware,
+	EnhancedStore,
+	ThunkAction,
+} from "@reduxjs/toolkit";
+
+import { createLogger } from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { createInjectorsEnhancer } from "redux-injectors";
+
+import createReducer from "./rootReducers";
+import { useMemo } from "react";
+
+export function configureAppStore(initialState = {}): EnhancedStore {
+	// Logger configuration
+	const logger = createLogger({
+		collapsed: true,
+		duration: true,
+	});
+
+	// Saga setup
+	const reduxSagaMonitorOptions = {};
+	const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
+	const { run: runSaga } = sagaMiddleware;
+
+	const middleware = [
+		...getDefaultMiddleware(),
+		logger,
+		sagaMiddleware, // Saga middleware injection
+	];
+
+	const enhancers = [
+		applyMiddleware(...middleware),
+		createInjectorsEnhancer({
+			createReducer,
+			runSaga,
+		}),
+	];
+
+	const store: Store = configureStore({
+		reducer: createReducer(),
+		preloadedState: initialState,
+		devTools: process.env.NODE_ENV !== "production",
+		enhancers,
+	});
+
+	return store;
+}
+
+const getStore = configureAppStore();
+export const rootReducers = createReducer();
+
+export type RootState = ReturnType<typeof rootReducers>;
+export type AppDispatch = typeof getStore.dispatch;
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default getStore;
+
+export function useStore(initialState: RootState = {}) {
+	const store = useMemo(() => configureAppStore(initialState), [
+		initialState,
+	]);
+	return store;
+}
